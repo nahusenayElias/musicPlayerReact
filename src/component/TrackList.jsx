5a5b6f3bef9f25d06fe767a74c156717d58c4bf5
Elias Hagos <eliasars@yahoo.com>
2024-08-29 21:04:49 +0300

    Fixes on TrackList

--- src/component/TrackList.jsx
+++ src/component/TrackList.jsx
@@ -2,19 +2,22 @@ import { useMusicPlayer } from "../hooks/useMusicPlayer";
 
 const TrackList = () => {
   const music = useMusicPlayer();
+
   return (
-    <div>
+    <>
       {music.trackList.map((track, index) => (
         <div key={`${track.name}_${index}`}>
           <button onClick={() => music.playTrack(index)}>
-            {music.isPlaying && music.currentTrackIndex === index
-              ? "Pause"
-              : "Play"}
+            {music.isPlaying && music.currentTrackIndex === index ? (
+               "Pause" 
+               ) : (
+              <span className="material-icons-outlined">play_arrow</span>
+               )}
           </button>
           <div>Track Playing: {track.name}</div>
         </div>
       ))}
-    </div>
+    </>
   );
 };
  
